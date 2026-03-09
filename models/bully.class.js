f8aaa8f3cb9a2bc0271e635f152bfffa6852b083
Jan-Oliver <jowieja22@gmail.com>
2026-03-09 18:03:49 +0100

    Objektorientierung im Projekt erlernt

---
+++ models/bully.class.js
@@ -0,0 +1,3 @@
+class Bully extends MoveableObject{
+
+}
---
+++ models/world.class.js
@@ -0,0 +1,12 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Bully(),
+        new Bully(),
+        new Bully()
+    ];
+
+    draw(){
+
+    }
+}
