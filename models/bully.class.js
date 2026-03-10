b8b86335a071c7312eecaaf041020493ee38db77
Jan-Oliver <jowieja22@gmail.com>
2026-03-10 21:20:48 +0100

    added rules für cloudes

--- models/bully.class.js
+++ models/bully.class.js
@@ -1,5 +1,7 @@
 class Bully extends MoveableObject{
     constructor(){
         super().loadImage('img/bully1/idle/idle-1.png');
+
+        this.x = 200 + Math.random() * 500;
     }
 }
---
+++ models/cloud.class.js
@@ -0,0 +1,12 @@
+class Cloud extends MoveableObject{
+    y = 50;
+    width = 500;
+    height = 250;
+
+
+    constructor(){
+        super().loadImage('img/clouds/clouds-combined.png');
+
+        this.x = Math.random() * 500; // Zahl zwischen 200 und 700
+    }
+}
