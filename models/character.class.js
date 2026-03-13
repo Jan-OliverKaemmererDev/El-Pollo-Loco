f24f86c48caa0357177d5d935a00d7e078d36ca0
Jan-Oliver <jowieja22@gmail.com>
2026-03-13 16:30:32 +0100

    changed walking pictures for character

--- models/character.class.js
+++ models/character.class.js
@@ -3,10 +3,12 @@ class Character extends MoveableObject {
     speed = 10;
 
     IMAGES_WALKING = [
-        'img/batman/idle/batman-idle1.png',
-        'img/batman/idle/batman-idle2.png',
-        'img/batman/idle/batman-idle3.png',
-        'img/batman/idle/batman-idle4.png'
+        'img/batman/walk/walk-1.png',
+        'img/batman/walk/walk-2.png',
+        'img/batman/walk/walk-3.png',
+        'img/batman/walk/walk-4.png',
+        'img/batman/walk/walk-5.png',
+        'img/batman/walk/walk-6.png'
     ];
     world;
 
